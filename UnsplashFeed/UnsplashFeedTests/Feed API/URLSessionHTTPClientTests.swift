b8ca85856fed6644f74a67dddaefa28ff3fbc67b
Christophe <christophe@wemoms.fr>
2023-11-07 07:54:04 +0100

    <refacto> [API] - Create MakeSUT factory methods to protect from futur breaking changes

--- UnsplashFeed/UnsplashFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ UnsplashFeed/UnsplashFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -50,7 +50,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -63,7 +63,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         
         let exp = expectation(description: "Wait for get completion")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, expectedError.domain)
@@ -80,6 +80,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private final class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
